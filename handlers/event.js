8739ca54a2ec678a208d7db293360d8f45d8503e
nona? <79404245+not-nonano@users.noreply.github.com>
2022-05-08 19:31:45 +0800

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,23 +1,18 @@
 const { readdirSync } = require("fs");
-module.exports =
-    /**
-     * 
-     * @param {Client} client 
-     */
-    (client) => {
-        try {
-            readdirSync("./events/").forEach((dir) => {
-                const eventFiles = readdirSync(`./events/${dir}/`).filter((file) => file.endsWith(".js"));
-                for (const file of eventFiles) {
-                    const event = require(`../events/${dir}/${file}`);
-                    if (event.once) {
-                        client.once(event.name, (...args) => event.execute(...args));
-                    } else {
-                        client.on(event.name, (...args) => event.execute(...args));
-                    }
+module.exports = (client) => {
+    try {
+        readdirSync("./events/").forEach((dir) => {
+            const eventFiles = readdirSync(`./events/${dir}/`).filter((file) => file.endsWith(".js"));
+            for (const file of eventFiles) {
+                const event = require(`../events/${dir}/${file}`);
+                if (event.once) {
+                    client.once(event.name, (...args) => event.execute(...args));
+                } else {
+                    client.on(event.name, (...args) => event.execute(...args));
                 }
-            });
-        } catch (e) {
-            console.log(e.stack)
-        }
+            }
+        });
+    } catch (e) {
+        console.log(e.stack)
     }
+}
